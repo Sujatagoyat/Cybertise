4d282c23334e9be0f321ea95eb554f0088b59272
Sujatagoyat <sujatagoyat8@gmail.com>
2024-04-16 11:49:52 +0530

    nav bar.hero-sec and data is safe sec done

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+const menulist = document.querySelector(".menulist")
+const menuIcon = document.querySelector(".menuIcon")
+
+menuIcon.addEventListener("click", function () {
+  menulist.classList.toggle("show");
+  menuIcon.classList.toggle("show");
+  document.body.classList.toggle("overFlowHidden")
+  document.querySelector(".menuIcon").classList.toggle("cross")
+})
