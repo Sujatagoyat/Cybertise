e37ae35c49e6ffbcf9498eef34639a1869c6e075
Sujatagoyat <sujatagoyat8@gmail.com>
2024-04-16 23:12:50 +0530

    aaded hover effect to nav links

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,16 @@ menuIcon.addEventListener("click", function () {
   document.body.classList.toggle("overFlowHidden")
   document.querySelector(".menuIcon").classList.toggle("cross")
 })
+const topBtn = document.querySelector(".topBtn");
+const show = document.querySelector(".show");
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 2000) {
+        topBtn.classList.add("show");
+    } else {
+        topBtn.classList.remove("show");
+    }
+})
+topBtn.addEventListener("click", function () {
+    document.documentElement.scrollTop = 0;
+})
